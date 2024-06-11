4b624c266e34c4ad0a982a16e7f69f0652942bdc
yuting <yuting09120310@gmail.com>
2024-06-11 11:08:49 +0800

    修改token

--- LineBot/Controllers/OrderCheckController.cs
+++ LineBot/Controllers/OrderCheckController.cs
@@ -11,7 +11,6 @@ namespace LineBot.Controllers
     {
         private readonly Bot _bot;
         private readonly IGoogleSheets _googleSheets;
-        private string _userId = "U0040d9605949cdceadab64168d00c335";
         private string _admin = "U41f5db0d177d113e24385ef4a8aba148";
 
         public OrderCheckController(Bot bot, IGoogleSheets googleSheets)
@@ -35,17 +34,6 @@ namespace LineBot.Controllers
                 if (request.ServiceDate.Date == threeDaysBefore)
                 {
                     // 發送通知
-                    _bot.PushMessage(_userId,
-                        @$"距離預約時間還有三天，請務必留意：
-訂單編號：{request.Id}
-訂單時間：{request.ServiceDate.ToString("yyyy-MM-dd")} {request.ServiceTime},
-個案大名：{request.FullName},
-上車地點：{request.PickupLocation},
-下車地點：{request.DropOffLocation},
-聯絡電話：{request.ContactPhoneNumber},
-服務項目：{request.ServiceType},
-備註：{request.Notes}");
-
                     _bot.PushMessage(_admin,
                         @$"距離預約時間還有三天，請留意：
 訂單編號：{request.Id}
